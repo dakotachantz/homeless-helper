f9baaadfe807a07e080fe3001c979832452ebf54
Dakota Nichols <dakotachantz@gmail.com>
2017-10-23 23:44:22 -0500

    edited styling of mobile nav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,26 +1,38 @@
 import React, { Component } from "react";
 import { stack as Menu } from "react-burger-menu";
 import { NavLink } from "react-router-dom";
+import logo from "../img/logo-white.png";
 export default class MobileNav extends Component {
   render() {
     return (
       <Menu right noOverlay>
+        <div
+          className="nav-logo"
+          style={{
+            margin: "2em",
+            display: "flex",
+            height: "1em",
+            justifyContent: "center"
+          }}
+        >
+          <NavLink
+            to="/"
+            className="logo-item"
+            style={{ height: "min-content" }}
+          >
+            <img src={logo} alt="" width="50" />
+          </NavLink>
+        </div>
         <ul className="mobile-ul">
-          <li>
-            <NavLink to="/" className="mn-has-sub active">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/findresources" className="mn-has-sub active">
-              Find Resources
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/ourmission" className="mn-has-sub active">
-              Our Mission
-            </NavLink>
-          </li>
+          <NavLink to="/" className="mn-has-sub active">
+            <li>Home</li>
+          </NavLink>
+          <NavLink to="/findresources" className="mn-has-sub active">
+            <li>Find Resources</li>
+          </NavLink>
+          <NavLink to="/ourmission" className="mn-has-sub active">
+            <li>Our Mission</li>
+          </NavLink>
         </ul>
       </Menu>
     );
